5311135958272b44c192e2b978359a05adb24433
Anup Adhikari <anupadhikari269@gmail.com>
2024-06-07 22:02:56 +0545

    added login

--- recipe/recipe/urls.py
+++ recipe/recipe/urls.py
@@ -7,7 +7,10 @@ from vege.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", receipes, name='receipes'),
-    path("delete_receipe/<id>/", delete_receipe, name='delete_receipes'), 
-    path("update_receipe/<id>/", update_receipe, name='update_receipes'),
-    path("receipes/", upload_receipes, name='upload_receipes'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Ensure this line is after urlpatterns definition
+    path("delete_receipe/<id>", delete_receipe, name='delete_receipes'), 
+    path("update_receipe/<id>", update_receipe, name='update_receipes'),
+    path("receipes", upload_receipes, name='upload_receipes'),
+    path("login", login_page, name='login'),
+    path("register", register_page, name='register'),
+    path("logout", logout_page, name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
--- recipe/vege/views.py
+++ recipe/vege/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Receipe
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login,logout,authenticate
 
 def receipes(request):
     if request.method == "POST":
@@ -67,3 +70,54 @@ def update_receipe(request, id):
 
     context = {'receipe': queryset}
     return render(request, 'updatereceipes.html', context)
+
+
+
+def login_page(request):
+
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        if not User.objects.filter(username=username).exists():
+            messages.error(request,'User doesnot exists')
+            return redirect('/register')
+        user =authenticate(username=username,password=password)
+
+        if user is None:
+            messages.error(request,'Invalid Credentials')
+            return redirect('/login')
+        else:
+            login(request,user)
+            return redirect('/')
+    return render(request,'login.html')
+
+
+
+def register_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.error(request, 'Username already exists')
+            return render(request, 'register.html', {'error_message': 'Username already exists'})
+
+        user = User.objects.create(
+            username=username,
+            email=email,
+        )
+
+        user.set_password(password)
+        user.save()
+        print(user.username)
+
+        return redirect('/login')
+
+    return render(request, 'register.html')
+
+def logout_page(request):
+    logout(request)
